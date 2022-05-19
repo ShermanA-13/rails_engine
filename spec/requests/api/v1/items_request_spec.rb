82146b8b30c6db4083285de23cdc8b9bc041675b
Sherman Au <shermau13@gmail.com>
2022-05-19 13:47:11 -0600

    test: sad path testing for items#show

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -20,31 +20,47 @@ RSpec.describe 'Items API' do
     end
   end
 
-  it 'returns one item' do
-    merchant = create(:merchant)
-    id = create(:item, merchant_id: merchant.id).id
+  context 'show' do
+    it 'returns one item' do
+      merchant = create(:merchant)
+      id = create(:item, merchant_id: merchant.id).id
 
-    get "/api/v1/items/#{id}"
+      get "/api/v1/items/#{id}"
 
-    item = JSON.parse(response.body, symbolize_names: true)
+      item = JSON.parse(response.body, symbolize_names: true)
 
-    expect(response).to be_successful
-    expect(response.status).to eq(200)
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
 
-    expect(item[:data]).to have_key(:id)
-    expect(item[:data][:id]).to be_an(String)
+      expect(item[:data]).to have_key(:id)
+      expect(item[:data][:id]).to be_an(String)
 
-    expect(item[:data][:attributes]).to have_key(:name)
-    expect(item[:data][:attributes][:name]).to be_an(String)
+      expect(item[:data][:attributes]).to have_key(:name)
+      expect(item[:data][:attributes][:name]).to be_an(String)
 
-    expect(item[:data][:attributes]).to have_key(:description)
-    expect(item[:data][:attributes][:description]).to be_an(String)
+      expect(item[:data][:attributes]).to have_key(:description)
+      expect(item[:data][:attributes][:description]).to be_an(String)
 
-    expect(item[:data][:attributes]).to have_key(:unit_price)
-    expect(item[:data][:attributes][:unit_price]).to be_an(Float)
+      expect(item[:data][:attributes]).to have_key(:unit_price)
+      expect(item[:data][:attributes][:unit_price]).to be_an(Float)
 
-    expect(item[:data][:attributes]).to have_key(:merchant_id)
-    expect(item[:data][:attributes][:merchant_id]).to be_an(Integer)
+      expect(item[:data][:attributes]).to have_key(:merchant_id)
+      expect(item[:data][:attributes][:merchant_id]).to be_an(Integer)
+    end
+
+    describe 'sad paths' do
+      it 'returns error if no item' do
+        merchant = create(:merchant)
+        id = create(:item, merchant_id: merchant.id).id
+
+        get "/api/v1/items/#{id + 1}"
+
+        parsed = JSON.parse(response.body, symbolize_names: true)
+        item = parsed[:data]
+
+        expect(item).to eq({ details: 'No item matches this id' })
+      end
+    end
   end
 
   it 'create one item' do
