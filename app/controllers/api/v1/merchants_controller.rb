7ae449f4249c2e2078f85889aa722e7f16b28423
Sherman Au <shermau13@gmail.com>
2022-05-19 09:53:15 -0600

    feat: merchant_controller#show

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::MerchantsController < ApplicationController
   def index
     render json: Merchant.all
   end
+
+  def show
+    render json: MerchantSerializer.new(Merchant.find(params[:id]))
+  end
 end
